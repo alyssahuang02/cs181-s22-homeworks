03e46cdb7f921b9a5bc60b92abe11b121d088dcf
Alyssa <alyssahuang@college.harvard.edu>
2022-04-29 21:35:36 -0400

    hw6 done

--- HW6/T6_P3/stub.py
+++ HW6/T6_P3/stub.py
@@ -2,6 +2,7 @@
 import numpy as np
 import numpy.random as npr
 import pygame as pg
+import matplotlib.pyplot as plt
 
 # uncomment this for animation
 # from SwingyMonkey import SwingyMonkey
@@ -21,10 +22,15 @@ class Learner(object):
     This agent jumps randomly.
     """
 
-    def __init__(self):
+    def __init__(self, alpha=0.2, gamma=0.9, starting_epsilon=0.1):
         self.last_state = None
         self.last_action = None
         self.last_reward = None
+        self.epoch = 0
+
+        self.alpha = alpha
+        self.gamma = gamma
+        self.starting_epsilon = starting_epsilon
 
         # We initialize our Q-value grid that has an entry for each action and state.
         # (action, rel_x, rel_y)
@@ -51,13 +57,36 @@ class Learner(object):
         Implement this function to learn things and take actions.
         Return 0 if you don't want to jump and 1 if you do.
         """
+        self.epoch += 1
+        # Hyperparameters
+        alpha = self.alpha
+        gamma = self.gamma
+        # epsilon = max(self.starting_epsilon - 0.001*self.epoch, 0)
+        epsilon = self.starting_epsilon
 
-        # TODO (currently monkey just jumps around randomly)
         # 1. Discretize 'state' to get your transformed 'current state' features.
-        # 2. Perform the Q-Learning update using 'current state' and the 'last state'.
-        # 3. Choose the next action using an epsilon-greedy policy.
+        rel_x, rel_y = self.discretize_state(state)
 
-        new_action = npr.rand() < 0.1
+        # 2. Perform the Q-Learning update using 'current state' and the 'last state'.
+        if self.last_state:
+            cur_max = np.max(self.Q[:, rel_x, rel_y])
+            old_rel_x, old_rel_y = self.discretize_state(self.last_state)
+            old_Q = self.Q[self.last_action, old_rel_x, old_rel_y]
+            updated_Q = (1 - alpha) * old_Q + alpha * (self.last_reward + gamma * cur_max)
+            self.Q[self.last_action, old_rel_x, old_rel_y] = updated_Q
+
+        # 3. Choose the next action using an epsilon-greedy policy.
+        if npr.uniform(0, 1) < epsilon:
+            # Explore
+            if npr.rand() < 0.5:
+                action = 0
+            else:
+                action = 1
+        else:
+            # Exploit
+            action = np.argmax(self.Q[:, rel_x, rel_y], axis=0)
+        
+        new_action = action
         new_state = state
 
         self.last_action = new_action
@@ -98,6 +127,25 @@ def run_games(learner, hist, iters=100, t_len=100):
 
 if __name__ == '__main__':
     # Select agent.
+
+    alphas = np.linspace(0.1, 0.3, num=3)
+    gammas = np.linspace(0.8, 1, num=3)
+    epsilons = np.linspace(0.1, 0.3, num=3)
+    num_trials = 10
+
+    # for alpha in alphas:
+    #     for gamma in gammas:
+    #         for epsilon in epsilons:
+    #             agent = Learner(alpha=alpha, gamma=gamma, starting_epsilon=epsilon)
+    #             maxes = 0
+    #             for trial in range(num_trials):
+    #                 hist = []
+    #                 run_games(agent, hist, 100, 100)
+    #                 maxes += max(hist)
+    #             print('alpha: ' + str(alpha) + 
+    #             ', gamma: ' + str(gamma) + ', epsilon: ' + str(epsilon) + 
+    #             ", avg max score: " + str(maxes/num_trials))
+
     agent = Learner()
 
     # Empty list to save history.
@@ -106,6 +154,14 @@ if __name__ == '__main__':
     # Run games. You can update t_len to be smaller to run it faster.
     run_games(agent, hist, 100, 100)
     print(hist)
+
+    plt.plot(range(100), hist)
+    plt.title("Scores over Epochs")
+    plt.xlabel("Epoch")
+    plt.ylabel("Score")
+    plt.tight_layout()
+    plt.savefig('scores_epoch.png')
+    plt.show()
 
     # Save history. 
     np.save('hist', np.array(hist))
